b98acc3d00f65dc641753dc220d2a609382fca23
frank zhang <frankbusinessmail@gmail.com>
2019-05-24 14:01:56 -0400

    header added

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <>
+            <h1>Sound like the smartest person in the room</h1>
+            <p>Curated videos that help you break the ice and have a great networking experience.</p>
+            <button>Getting Started</button>
+        </>
+    )
+}
+
+export default Header;
---
+++ client/src/Components/MainVideo/MainVideo.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class MainVideo extends Component{
+    render(){
+        return (
+            <>
+                <video src="https://download.ted.com/talks/ZeFrank_2004-light.mp4" controls></video>
+            </>
+        )
+    }
+}

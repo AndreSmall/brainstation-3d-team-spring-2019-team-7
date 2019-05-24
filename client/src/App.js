232f77742fd5dce0c454f43e4b03c50d5734f5bf
arshad1416 <arshad14@gmail.com>
2019-05-24 16:13:16 -0400

    Header section complete.

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.scss';
 import Header from './Components/Header/Header';
 import MainVideo from './Components/MainVideo/MainVideo';
 import Main from './Components/Main/Main';
+import Query from './Components/Query/Query';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Main />
+      <Query />
     </div>
   );
 }
